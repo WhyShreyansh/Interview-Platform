44830c180507a28f343f8d37a42c762d635982e1
shreyansh <shreyanshdev88@gmail.com>
2026-08-12 16:10:53 +0530

    update

--- interview-platform/src/components/ui/card.tsx
+++ interview-platform/src/components/ui/card.tsx
@@ -33,3 +33,10 @@ const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDi
   ({ className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 );
 CardContent.displayName = "CardContent";
+
+const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
+  )
+);
+CardFooter.displayName = "CardFooter";
