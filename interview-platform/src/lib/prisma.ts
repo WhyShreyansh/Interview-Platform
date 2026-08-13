94c2a18345e7a5449f9e88ae92f3d21e1d8c9fde
shreyansh <shreyanshdev88@gmail.com>
2026-08-13 11:21:41 +0530

    update

--- interview-platform/src/lib/prisma.ts
+++ interview-platform/src/lib/prisma.ts
@@ -7,3 +7,7 @@ export const prisma =
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
+
+  if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
