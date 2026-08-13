087d204ebf7ccc35535fb1f15e477a290542f83c
shreyansh <shreyanshdev88@gmail.com>
2026-08-13 11:14:18 +0530

    code - ui/select.tsx

---
+++ interview-platform/src/components/ui/select.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import * as React from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { Check, ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const Select = SelectPrimitive.Root;
+const SelectValue = SelectPrimitive.Value;
+const SelectGroup = SelectPrimitive.Group;
