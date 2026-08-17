c2e77f776a0ea11a0237f8a2d5f5d76a9512803a
shreyansh <shreyanshdev88@gmail.com>
2026-08-17 13:32:52 +0530

    starting whiteboard.tsx code

---
+++ interview-platform/src/features/room/components/Whiteboard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useMemo, useRef, useState } from "react";
+import dynamic from "next/dynamic";
+import type { Socket } from "socket.io-client";
+import type { ClientToServerEvents, ServerToClientEvents } from "@/features/room/types/events";
+import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
+import "@excalidraw/excalidraw/index.css";
+
+type AppSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
+
+const Excalidraw = dynamic(
+  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+  { ssr: false }
+);
